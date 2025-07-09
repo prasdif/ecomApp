0232affdab2ed51d41b6ca6350a62f08f4e27715
Prasad Borse <prasadborse433@gmail.com>
2025-07-09 10:46:50 +0000

    Initial commit

---
+++ src/components/CartSummary/index.js
@@ -0,0 +1,37 @@
+import CartContext from '../../context/CartContext'
+
+import './index.css'
+
+const CartSummary = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList} = value
+      const total = cartList.reduce(
+        (sum, item) => sum + item.price * item.quantity,
+        0,
+      )
+
+      return (
+        <>
+          <div className="cart-summary-container">
+            <h1 className="order-total-value">
+              <span className="order-total-label">Order Total:</span> Rs {total}{' '}
+              /-
+            </h1>
+            <p className="total-items">{cartList.length} Items in cart</p>
+            <button type="button" className="checkout-button d-sm-none">
+              Checkout
+            </button>
+          </div>
+          <button type="button" className="checkout-button d-lg-none">
+            Checkout
+          </button>
+        </>
+      )
+    }}
+  </CartContext.Consumer>
+)
+
+export default CartSummary
+
+// Write your code here
